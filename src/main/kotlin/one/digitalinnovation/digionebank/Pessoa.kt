5b414b481106dc28ddb00afb9856efd222808acc
Felipe Oliveira <flpeolivier@gmail.com>
2021-07-16 10:45:42 -0300

    updated to apply the private modifier to set function of cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,12 +2,18 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Felipe"
+
     var cpf: String = "454.958.098-43"
+    private set
+    //privando o método set para não ser possível alterar seu valor
 }
 
 fun main() {
     val felipe = Pessoa()
 
+    //felipe.cpf = "1892738"
+
     println(felipe.nome)
-    print(felipe.cpf)
+    println(felipe.cpf)
+
 }
