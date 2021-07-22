52c807dcea508d20aa572027869069118116e129
Felipe Oliveira <flpeolivier@gmail.com>
2021-07-22 11:12:13 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,20 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Felipe"
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
 
-    var cpf: String = "454.958.098-43"
-    private set
-    //privando o método set para não ser possível alterar seu valor
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val felipe = Pessoa()
-
-    println(felipe.pessoaInfo())
-
-}
